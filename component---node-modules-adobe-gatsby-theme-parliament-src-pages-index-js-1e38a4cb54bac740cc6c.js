f109976aa366ba747e0f42b2d6b914d81a1d4b0b
Stephan Ringel <ringel@adobe.com>
2020-07-30 18:33:44 +0200

    Publish

---
+++ component---node-modules-adobe-gatsby-theme-parliament-src-pages-index-js-1e38a4cb54bac740cc6c.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"9Kv+":function(e,t,n){"use strict";n.r(t);var r=n("q1tI"),b=n("Wbzz"),a=n("rid2"),c=n("P2y5"),i=n("qKvR");t.default=function(){var e=Object(r.useContext)(c.b).siteMetadata,t=Object(b.withPrefix)(e.pages[0].path);return Object(i.b)(a.HelmetProvider,null,Object(i.b)(a.Helmet,null,Object(i.b)("meta",{"http-equiv":"refresh",content:"0; url="+t})))}}}]);
+//# sourceMappingURL=component---node-modules-adobe-gatsby-theme-parliament-src-pages-index-js-1e38a4cb54bac740cc6c.js.map
