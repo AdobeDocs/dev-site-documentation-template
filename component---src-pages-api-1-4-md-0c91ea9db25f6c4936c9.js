d8db17d167bf397fc129db47933a15f53cf6f377
icaraps <ringel@adobe.com>
2022-04-28 11:15:26 +0000

    Deploying to gh-pages from  @ df75c15cc80eea457356ce1c297ee8664b773fba 🚀

---
+++ component---src-pages-api-1-4-md-0c91ea9db25f6c4936c9.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdev_site_documentation_template=self.webpackChunkdev_site_documentation_template||[]).push([[555],{14166:function(t,n,e){e.r(n),e.d(n,{_frontmatter:function(){return m},default:function(){return p}});var o=e(87462),u=e(63366),r=(e(15007),e(64983)),a=e(91515),c=["components"],m={},s={_frontmatter:m},i=a.Z;function p(t){var n=t.components,e=(0,u.Z)(t,c);return(0,r.mdx)(i,(0,o.Z)({},s,e,{components:n,mdxType:"MDXLayout"}))}p.isMDXComponent=!0}}]);
+//# sourceMappingURL=component---src-pages-api-1-4-md-0c91ea9db25f6c4936c9.js.map
