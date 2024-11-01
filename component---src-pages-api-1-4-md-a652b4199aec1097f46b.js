2b24a19f337839b53eae3241854bce3eef54bf97
timkim <timk@adobe.com>
2024-11-01 21:21:26 +0000

    Deploying to gh-pages from @ AdobeDocs/dev-site-documentation-template@eb271b1c9ca040a840532b222ae120ed556a63b5 🚀

---
+++ component---src-pages-api-1-4-md-a652b4199aec1097f46b.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdev_site_documentation_template=self.webpackChunkdev_site_documentation_template||[]).push([[436],{84753:function(t,n,e){e.r(n),e.d(n,{_frontmatter:function(){return s},default:function(){return p}});var o=e(58168),u=e(80045),c=(e(88763),e(15680)),r=e(83407);const m=["components"],s={},a={_frontmatter:s},i=r.A;function p(t){let{components:n}=t,e=(0,u.A)(t,m);return(0,c.mdx)(i,(0,o.A)({},a,e,{components:n,mdxType:"MDXLayout"}))}p.isMDXComponent=!0}}]);
+//# sourceMappingURL=component---src-pages-api-1-4-md-a652b4199aec1097f46b.js.map
---
+++ component---src-pages-api-index-md-63f9e9b37ab49d9b8089.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdev_site_documentation_template=self.webpackChunkdev_site_documentation_template||[]).push([[542],{40816:function(t,n,e){e.r(n),e.d(n,{_frontmatter:function(){return s},default:function(){return p}});var o=e(58168),u=e(80045),c=(e(88763),e(15680)),r=e(83407);const m=["components"],s={},a={_frontmatter:s},i=r.A;function p(t){let{components:n}=t,e=(0,u.A)(t,m);return(0,c.mdx)(i,(0,o.A)({},a,e,{components:n,mdxType:"MDXLayout"}))}p.isMDXComponent=!0}}]);
+//# sourceMappingURL=component---src-pages-api-index-md-63f9e9b37ab49d9b8089.js.map
